08490673ca0da6ec32d309a41a6ddc0ca54074f2
Mackenzie <ma.luzarow@gmail.com>
2017-09-25 20:57:28 -0500

    Plottin revenge

--- jKeyzie.js
+++ jKeyzie.js
@@ -31,6 +31,14 @@ function HTMLBuildImage (source, alternate = null) {
     }
 }
 
+/**
+ * A coordinate pair (x, y) on a cartesian plane.
+ *
+ * @param  {float} x X-axis value.
+ * @param  {float} y Y-axis value.
+ *
+ * @constructor
+ */
 function Coordinate2D (x, y) {
     this.x = x;
     this.y = y;
@@ -77,15 +85,62 @@ function Plotter (containerElement, rows, columns) {
 
     this.plots = t;
 
+    this.Plot = function (data, Xmax = null, Xmin = null, Ymax = null, Ymin = null) {
+        this.data = data;
+        this.Xmax = Xmax;
+        this.Xmin = Xmin;
+        this.Xdelta = 0;
+        this.Ymax = Ymax;
+        this.Ymin = Ymin;
+        this.Ydelta = 0;
+
+        this.findAxisSpacing = function () {
+            // Grab all x values and sort them
+            var Xvalues = new Array ();
+            var Yvalues = new Array ();
+            for (var i = 0; i < this.data.length; i++) {
+                Xvalues [i] = this.data [i].x;
+                Yvalues [i] = this.data [i].y;
+            }
+            Xvalues.sort ();
+            Yvalues.sort ();
+
+            if (this.Xmin == null) {
+                this.Xmin = Xvalues [0];
+            }
+            if (this.Xmax == null) {
+                this.Xmax = Xvalues [Xvalues.length - 1];
+            }
+            if (this.Ymin == null) {
+                this.Ymin = Yvalues [0];
+            }
+            if (this.Ymax == null) {
+                this.Ymax = Yvalues [Yvalues.length - 1];
+            }
+        }
+
+        this.findAxisSpacing ();
+    }
+
+    /**
+     * Create a new Plot.
+     *
+     * @param  {Array}  data  Array of data in the form of coordinates
+     *
+     * @return  {Plot}  The created Plot.
+     */
+    this.newPlot = function (data) {
+        return (new Plot (data))
+    }
+
+    /**
+     * Add a plot to the plotter layout in the given slot.
+     *
+     * @param  {Plot}  plot    The plot to be added.
+     * @param  {int}   row     The row into which the plot should be inserted.
+     * @param  {int}   column  The column into which the plot should be inserted.
+     */
     this.addPlot = function (plot, row, column) {
         this.plots [row][column] = plot;
-    }
-
-    this.newPlot = function (data) {
-        return (new Plot ())
-    }
-
-    this.Plot = function () {
-
     }
 }
